6bf0112ef586301907fff7862de7c229844ae6ca
gaeun <knospe1@naver.com>
2022-06-14 23:28:41 +0900

    converter, controller 추가

---
+++ src/main/java/com/example/interfaceimplprac/Converter.java
@@ -0,0 +1,14 @@
+package com.example.interfaceimplprac;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Converter {
+
+    public void converter(TestInterface testInterface) {
+        testInterface.save();
+    }
+
+}
